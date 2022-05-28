f03105b02ac6400932def3eba1dc831e975aa2e1
Mirek Długosz <mirek@mirekdlugosz.com>
2022-05-28 23:53:30 +0200

    get_version.py Python 3.6 compatibility fixes

--- roles/system_requirements/files/get_version.py
+++ roles/system_requirements/files/get_version.py
@@ -5,9 +5,16 @@ import sys
 import urllib.request
 from datetime import datetime
 
+LATEST_STR = "latest"
+gh_date_format = "%Y-%m-%dT%H:%M:%SZ"
+
+
+def get_release_date(release_obj):
+    date_str = release_obj.get("published_at")
+    return datetime.strptime(date_str, gh_date_format)
+
 
 def main():
-    LATEST_STR = "latest"
     parser = argparse.ArgumentParser(
         description="Get Kustosz component version from GitHub API"
     )
@@ -35,10 +42,11 @@ def main():
 
     latest_release = data[0]
     for release in data:
-        latest_release_dt = datetime.fromisoformat(release.get('published_at').strip('Z'))
-        this_release_dt = datetime.fromisoformat(release.get('published_at').strip('Z'))
+        latest_release_dt = get_release_date(latest_release)
+        this_release_dt = get_release_date(release)
         if this_release_dt > latest_release_dt:
             latest_release = release
+
     print(latest_release.get('tag_name'))
 
 
